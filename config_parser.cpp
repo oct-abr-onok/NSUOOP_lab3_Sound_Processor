df68f831d6b758f833aaa3f25a3febae3ff68346
oct-abr-onok <oct.abr.onok@gmail.com>
2023-02-14 00:23:12 +0700

    added config_parser base

---
+++ config_parser.cpp
@@ -0,0 +1,26 @@
+#include "config_parser.h"
+
+config_parser::config_parser(std::string file_name) : _file_name(file_name)
+{
+	std::string buffer;
+
+	std::ifstream fin(_file_name);
+	while (true)
+	{
+		std::getline(fin, buffer);
+		if (buffer[0] == '#')
+		{
+			continue;
+		}
+		else
+		{
+
+		}
+	}
+}
+
+command config_parser::pop_command()
+{
+	command command;
+	
+}
---
+++ config_parser.h
@@ -0,0 +1,26 @@
+#ifndef CONFIG_PARSER_76
+#define CONFIG_PARSER_76
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+#include "converters.h"
+
+struct command
+{
+	std::string name;
+	int p1, p2;
+};
+
+class config_parser
+{
+private:
+	std::string _file_name;
+	std::vector<command> _commands;
+public:
+	config_parser(std::string file_name);
+	command pop_command(); //возвращает комманду
+};
+
+#endif /*CONFIG_PARSER_76*/
