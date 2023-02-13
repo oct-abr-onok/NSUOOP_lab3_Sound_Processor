f51eba3c3793b747de918ec84293228a3ab4b368
oct-abr-onok <oct.abr.onok@gmail.com>
2023-02-13 22:30:17 +0700

    command line parser added

--- main.cpp
+++ main.cpp
@@ -1,6 +1,38 @@
 #include "main.h"
 
-int main()
+int main(int argc, char* argv[])
 {
+	//парсер команд
+	bool helper = false;
+	std::string config_file, output_file;
+	std::vector<std::string> input_files;
+
+	std::string buffer;
+	bool is_c = false;
+	for (int i = 1; i < argc; i++)
+	{
+		buffer = std::string(argv[i]);
+		if (buffer == "-c")
+		{
+			config_file = std::string(argv[++i]);
+			output_file = std::string(argv[++i]);
+			input_files.push_back(std::string(argv[++i]));
+			is_c = true;
+			continue;
+		}
+		else if (buffer == "-h")
+		{
+			helper = true;
+			continue;
+		}
+			if (is_c)
+		{
+			input_files.push_back(std::string(argv[i]));
+			continue;
+		}
+	}
+
+
+
 	return 0;
 }
--- main.h
+++ main.h
@@ -1,10 +1,10 @@
-#define MAIN_47
 #ifndef MAIN_47
+#define MAIN_47
 
 #include <tuple>
 #include <fstream>
 #include <iostream>
-#include <iomanip>
 #include <string>
+#include <vector>
 
 #endif /*MAIN_47*/
